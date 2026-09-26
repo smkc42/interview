18d02939e1ed4bcd7ad60266a58800695a6eb3b4
Soumik Chatterjee <smkc42@proton.me>
2026-09-26 21:43:01 +0530

    feat: finish week4 problems

--- dsa/grind75/week4.py
+++ dsa/grind75/week4.py
@@ -3,6 +3,90 @@ from collections import deque
 from dsa.datastructures import ListNode, TreeNode
 
 
+# https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+def lowest_common_ancestor(root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+    def util(node: TreeNode | None) -> tuple[TreeNode | None, bool, bool]:
+        if node is None:
+            return (None, False, False)
+        left, leftp, leftq = util(node.left)
+        if leftp and leftq:
+            return (left, True, True)
+        right, rightp, rightq = util(node.right)
+        if rightp and rightq:
+            return (right, True, True)
+        has_p = node == p or leftp or rightp
+        has_q = node == q or leftq or rightq
+        return (node if has_p and has_q else None, has_p, has_q)
+
+    node, has_p, has_q = util(root)
+    assert has_p and has_q and node is not None
+    return node
+
+
+# https://leetcode.com/problems/merge-intervals/
+def merge(intervals: list[list[int]]) -> list[list[int]]:
+    """
+    >>> merge([[1, 3], [2, 6], [8, 10], [15, 18]])
+    [[1, 6], [8, 10], [15, 18]]
+    >>> merge([[1, 4], [4, 5]])
+    [[1, 5]]
+    >>> merge([[4, 7], [1, 4]])
+    [[1, 7]]
+    """
+    intervals.sort()
+    merged: list[list[int]] = []
+    cur_start, cur_end = intervals[0]
+    for start, end in intervals[1:]:
+        if start > cur_end:
+            merged.append([cur_start, cur_end])
+            cur_start, cur_end = start, end
+        else:
+            cur_end = max(cur_end, end)
+    merged.append([cur_start, cur_end])
+    return merged
+
+
+# https://leetcode.com/problems/permutations/
+def permute(nums: list[int]) -> list[list[int]]:
+    """
+    >>> permute([1, 2, 3])
+    [[1, 2, 3], [2, 1, 3], [2, 3, 1], [1, 3, 2], [3, 1, 2], [3, 2, 1]]
+    >>> permute([0, 1])
+    [[0, 1], [1, 0]]
+    >>> permute([1])
+    [[1]]
+    """
+    if len(nums) <= 1:
+        return [nums]
+    rest = permute(nums[1:])
+    res: list[list[int]] = []
+    for perm in rest:
+        for pos in range(len(perm) + 1):
+            res.append(perm[:pos] + [nums[0]] + perm[pos:])
+    return res
+
+
+# https://leetcode.com/problems/combination-sum/
+def combination_sum(candidates: list[int], target: int) -> list[list[int]]:
+    """
+    >>> combination_sum([2, 3, 6, 7], 7)
+    [[7], [2, 2, 3]]
+    >>> combination_sum([2, 3, 5], 8)
+    [[3, 5], [2, 3, 3], [2, 2, 2, 2]]
+    """
+    if len(candidates) == 0:
+        return [[]] if target == 0 else []
+    cur: list[int] = []
+    res: list[list[int]] = []
+    while (cursum := sum(cur)) <= target:
+        rest = combination_sum(candidates[1:], target - cursum)
+        if len(rest) > 0:
+            for r in rest:
+                res.append(cur + r)
+        cur.append(candidates[0])
+    return res
+
+
 # https://leetcode.com/problems/search-in-rotated-sorted-array/
 def search(nums: list[int], target: int) -> int:
     """
